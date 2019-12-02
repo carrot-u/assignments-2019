e6628d9b42e696a4de6873477befca80510937b9
Robert Border <robert.border@instacart.com>
2019-12-02 17:28:22 -0500

    Complete base logic in Tiny Instacart app

---
+++ assignment-2-ruby/rborder/order_items_data.rb
@@ -0,0 +1,56 @@
+AISLES = ['Produce','Dairy & Eggs','Meat & Seafood','Snacks','Deli','Frozen Foods','Bulk','Household','Bakery']
+
+ITEMS = [{name: "Lettuce",  aisle: "Produce",   cost: 1},
+         {name: "Broccoli", aisle: "Produce",   cost: 1},
+         {name: "Apples",   aisle: "Produce",   cost: 1},
+         {name: "Oranges",  aisle: "Produce",   cost: 1},
+         {name: "Bananas",  aisle: "Produce",   cost: 1},
+         {name: "Carrots",  aisle: "Produce",   cost: 1},
+
+         {name: "Eggs",     aisle: "Dairy & Eggs", cost: 2},
+         {name: "Milk",     aisle: "Dairy & Eggs", cost: 3},
+         {name: "Yogurt",   aisle: "Dairy & Eggs", cost: 3},
+         {name: "Creamer",  aisle: "Dairy & Eggs", cost: 1},
+         {name: "Butter",   aisle: "Dairy & Eggs", cost: 1},
+
+         {name: "Chicken",    aisle: "Meat & Seafood", cost: 6},
+         {name: "Steak",      aisle: "Meat & Seafood", cost: 7},
+         {name: "Salmon",     aisle: "Meat & Seafood", cost: 8},
+         {name: "Shrimp",     aisle: "Meat & Seafood", cost: 8},
+         {name: "Ground Beef", aisle: "Meat & Seafood", cost: 3},
+
+         {name: "Chips",   aisle: "Snacks", cost: 1},
+         {name: "Nuts",    aisle: "Snacks", cost: 2},
+         {name: "Granola", aisle: "Snacks", cost: 1},
+         {name: "Popcorn", aisle: "Snacks", cost: 1},
+         {name: "Candy",   aisle: "Snacks", cost: 1},
+
+         {name: "Turkey",     aisle: "Deli", cost: 3},
+         {name: "Roast Beef", aisle: "Deli", cost: 3},
+         {name: "Salami",     aisle: "Deli", cost: 3},
+         {name: "Cheese",     aisle: "Deli", cost: 3},
+         {name: "Pub Sub",    aisle: "Deli", cost: 5, "warehouse_restriction": "Publix"},
+
+         {name: "Frozen Vegetables", aisle: "Frozen Foods", cost: 2},
+         {name: "Frozen Pizza",      aisle: "Frozen Foods", cost: 4},
+         {name: "Frozen Meals",      aisle: "Frozen Foods", cost: 2},
+         {name: "Frozen Fruit",      aisle: "Frozen Foods", cost: 2},
+         {name: "Ice Cream",         aisle: "Frozen Foods", cost: 3},
+
+         {name: "Rice",     aisle: "Bulk", cost: 2},
+         {name: "Quinoa",   aisle: "Bulk", cost: 5},
+         {name: "Oats",     aisle: "Bulk", cost: 2},
+         {name: "Couscous", aisle: "Bulk", cost: 4},
+         {name: "Coffee",   aisle: "Bulk", cost: 8},
+
+         {name: "Detergent",    aisle: "Household", cost: 7},
+         {name: "Paper Towels", aisle: "Household", cost: 2},
+         {name: "Soap",         aisle: "Household", cost: 1},
+         {name: "Trash Bags",   aisle: "Household", cost: 2},
+         {name: "Aluminum Foil", aisle: "Household", cost: 5},
+
+         {name: "Rolls",  aisle: "Bakery", cost: 1},
+         {name: "Muffin", aisle: "Bakery", cost: 3},
+         {name: "Bread",  aisle: "Bakery", cost: 1},
+         {name: "Donuts", aisle: "Bakery", cost: 3},
+         {name: "Cake",   aisle: "Bakery", cost: 10}]
--- assignment-2-ruby/rborder/tiny_instacart.rb
+++ assignment-2-ruby/rborder/tiny_instacart.rb
@@ -18,18 +18,39 @@ def select_warehouse
   return selected_store
 end
 
+def aisle_prompt
+  puts "\nBrowse departments:"
+  AISLES.each do |department|
+    print "| #{department} |"
+  end
+  puts "\n" * 2
+end
+
 def order
   workflow_state = "shopping"
   order_items = []
-  puts "\nBrowse departments:"
-  AISLES.each do |department|
-    puts department + " | "
-  end
+  aisle_prompt
   response = gets.chomp
+  puts "Use 'q' to exit the order building process.\n"
+  until response == "q"
+    if AISLES.include?(response)
+      puts "Select from the following in stock items: "
+      ITEMS.each do |item|
+        puts "#{item[:name]}" if item[:aisle] == response
+      end
+      order_items.append(gets.chomp)
+    end
+    puts "\n"
+    line_break
+    puts " Select another aisle to browse or enter 'q' to exit: "
+    aisle_prompt
+    response = gets.chomp
+  end
+  return order_items
 end
 
 def line_break
-  "---------------------------"
+  puts "---------------------------"
 end
 
 
@@ -40,7 +61,7 @@ def delivery_window
     puts "Delivery within #{i} hour/s."
   end
   while deliv < 1
-    print "Select from an available time: "
+    print "\nSelect from an available time (int): "
     deliv = gets.chomp.to_i
   end
   return deliv
@@ -52,20 +73,46 @@ end
 
 # delivery or pickup choice
 def service_selection
-  puts "Select a delivery time."
   service = delivery_window
   t = Time.now + to_h(service)
   puts "Your order will arrive by #{t}"
   return t
 end
 
+def shopping(list)
+  puts "Your shopper is now shopping your order."
+  found_items = []
+  unfound_items = []
+  item_names = ITEMS.collect { |i| i[:name] }
+  for item in list
+    if item_names.include?(item)
+      found_items.append(item)
+    else
+      unfound_items.append(item)
+    end
+  end
+  unfound_items.each do |item|
+    puts "#{item} could not be found. If a suitable replacement does not exist, the item will be refunded."
+  end
+  return found_items
+end
+
 def customer_app
-  puts line_break + "Welcome to Instacart!" + line_break
+  puts ("*" * 20) + "Welcome to Instacart!" + ("*" * 20)
   puts "\nPlease select a store from available warehouses in your area: "
   warehouse = select_warehouse
+  sleep 0.5
+  line_break
   modality = service_selection
-  puts "warehouse: #{warehouse}"
-  puts "service: #{modality}"
+  sleep 0.5
+  line_break
+  basket = order
+  sleep 0.5
+  line_break
+  found = shopping(basket)
+  sleep 0.5
+  line_break
+  puts "Shopping is complete.\nYour order from #{warehouse} is scheduled to be delivered at #{modality}"
 end
 
 # -------------------------------------------------
