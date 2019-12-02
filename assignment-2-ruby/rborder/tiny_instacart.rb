ce112677993f6232cec172c45a88be5ef8cce629
Robert Border <robert.border@instacart.com>
2019-12-02 14:58:21 -0500

    Update tiny instacart program

--- assignment-2-ruby/rborder/tiny_instacart.rb
+++ assignment-2-ruby/rborder/tiny_instacart.rb
@@ -1,26 +1,5 @@
-# SHOPPER APP
-
-# presented with possible batches
-def accept_batch
-  return nil
-end
-
-# shopping animation and replacements
-def shop
-  return nil
-end
-
-# driving animation and scan bags?
-def deliver
-  return nil
-end
-
-# run shopper workflow
-def shopper_app
-  return nil
-end
-
-# --------------------------------------------------
+require './order_items_data'
+require 'time'
 
 # CUSTOMER APP
 
@@ -29,34 +8,65 @@ def select_warehouse
   selected_store = ""
   stores = ['Kroger','Publix','Aldi','Sprouts']
   stores.each do |store|
-    puts "#{store}"
+    print "| #{store} |"
   end
-  while stores.downcase.include?(selected_store) == false do
-    puts "Select an available store"
-    selected_store = gets.chomp.downcase
-    if selected_store
-
-    end
+  puts "\n"
+  while stores.include?(selected_store) == false do
+    print "\nSelect an available store: "
+    selected_store = gets.chomp
   end
+  return selected_store
 end
 
 def order
-  return nil
+  workflow_state = "shopping"
+  order_items = []
+  puts "\nBrowse departments:"
+  AISLES.each do |department|
+    puts department + " | "
+  end
+  response = gets.chomp
+end
+
+def line_break
+  "---------------------------"
+end
+
+
+# picking a delivery window
+def delivery_window
+  deliv = 0
+  (1..5).each do |i|
+    puts "Delivery within #{i} hour/s."
+  end
+  while deliv < 1
+    print "Select from an available time: "
+    deliv = gets.chomp.to_i
+  end
+  return deliv
+end
+
+def to_h(n)
+  return n * 60 * 60
 end
 
 # delivery or pickup choice
 def service_selection
-  return nil
+  puts "Select a delivery time."
+  service = delivery_window
+  t = Time.now + to_h(service)
+  puts "Your order will arrive by #{t}"
+  return t
 end
 
 def customer_app
-  return nil
+  puts line_break + "Welcome to Instacart!" + line_break
+  puts "\nPlease select a store from available warehouses in your area: "
+  warehouse = select_warehouse
+  modality = service_selection
+  puts "warehouse: #{warehouse}"
+  puts "service: #{modality}"
 end
 
 # -------------------------------------------------
-
-def select_workflow
-  puts ""
-end
-
-select_warehouse
+customer_app
