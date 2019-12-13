7479166ee725e05a18c91980fef620099d9138a3
bayoblackman <leselinyana.blackman@instacart.com>
2019-12-13 17:36:21 -0500

    updated assignment 1(Bonus Questions)

--- assignment-1-sql/bayoblackman.sql
+++ assignment-1-sql/bayoblackman.sql
@@ -4,3 +4,25 @@ WHERE charge_amount > 500
 AND completed_at IS NOT NULL 
 ORDER BY completed_at DESC
 limit 1;
+
+-- Bonus Assignment
+-- Question 1
+select id, email
+From users
+where email = 'leselinyana.blackman@instacart.com';
+-- Question 2
+select count(user_id)
+from orders
+Where user_id = '22438411';
+-- Question 3
+select id, reconciled_total, completed_at
+from orders
+WHERE reconciled_total > 500 
+AND completed_at IS NOT NULL
+Order by completed_at DESC
+limit 5;
+-- Question 4
+select count(*)
+from order_deliveries
+where warehouse_id = 1000
+and delivered_at > dateadd('day',-1,now());
